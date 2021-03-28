db1aad50e8f22320660e51e84f88515c7535e1ee
Gioele Farina <jchilosa@gmail.com>
2021-03-28 20:17:48 +0200

    ingredients PropTypes

--- src/components/Burger/Ingredient/Ingredient.js
+++ src/components/Burger/Ingredient/Ingredient.js
@@ -1,42 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import classes from './Ingredient.css';
 import Wrapper from '../../Wrapper/Wrapper';
 
-const ingredient = (props) => {
+class Ingredient extends Component {
 
-  let renderedIng = null;
+  render(){
+    let renderedIng = null;
 
-  switch (props.type)
-  {
-   case 'BreadTop':
-     renderedIng =
-     <div className={classes.BreadTop}>
-       <div className={classes.Seeds1}></div>
-       <div className={classes.Seeds2}></div>
-     </div>;
-     break;
-   case 'BreadBottom':
-    renderedIng = <div className={classes.BreadBottom}></div>;
-    break;
-   case 'Meat':
-    renderedIng = <div className={classes.Meat}></div>;
-    break;
-   case 'Cheese':
-    renderedIng = <div className={classes.Cheese}></div>;
-    break;
-   case 'Salad':
-    renderedIng = <div className={classes.Salad}></div>;
-    break;
-   case 'Bacon':
-    renderedIng = <div className={classes.Bacon}></div>;
-    break;
+    switch (this.props.type)
+    {
+     case 'BreadTop':
+       renderedIng =
+       <div className={classes.BreadTop}>
+         <div className={classes.Seeds1}></div>
+         <div className={classes.Seeds2}></div>
+       </div>;
+       break;
+     case 'BreadBottom':
+      renderedIng = <div className={classes.BreadBottom}></div>;
+      break;
+     case 'Meat':
+      renderedIng = <div className={classes.Meat}></div>;
+      break;
+     case 'Cheese':
+      renderedIng = <div className={classes.Cheese}></div>;
+      break;
+     case 'Salad':
+      renderedIng = <div className={classes.Salad}></div>;
+      break;
+     case 'Bacon':
+      renderedIng = <div className={classes.Bacon}></div>;
+      break;
+    }
+
+    return (
+      <Wrapper>
+        {renderedIng}
+      </Wrapper>
+    );
   }
-
-  return (
-    <Wrapper>
-      {renderedIng}
-    </Wrapper>
-  );
 }
 
-export default ingredient;
+Ingredient.propTypes = {
+  type: PropTypes.string.isRequired
+}
+
+export default Ingredient;
